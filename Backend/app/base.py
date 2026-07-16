a908a3a72595ebf76f27aeef42fcd78418aa298d
MatveyFilippov <matdufi2004@gmail.com>
2026-07-16 20:28:11 +0300

    feat(backend): design base app

---
+++ Backend/app/base.py
@@ -0,0 +1,38 @@
+import logging
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+
+
+APP = FastAPI(version="1.0.0", title="SleepWarden")
+log = logging.getLogger("app.base")
+
+
+@APP.middleware("http")
+async def log_middleware(request: Request, call_next):
+    log.debug((
+        f"REQUEST: {request.method} {request.url.path}"
+        f" (Requester:{request.client};"
+        f"Headers:{request.headers.raw};"
+        f"QueryParams:{request.query_params};"
+        f"Body:{await request.body()})"
+    ))
+    response = await call_next(request)
+    log.debug(f"RESPONSE: {response.status_code}")
+    return response
+
+
+@APP.exception_handler(Exception)
+async def global_exception_handler(request: Request, ex: Exception):
+    log.error(f"Faced with unhandled exception: {ex}", exc_info=True)
+    log.debug("RESPONSE: 500")
+    return JSONResponse(
+        content={"detail": "Internal server error"},
+        status_code=500,
+    )
+
+
+def start(host: str, port: int):
+    log.debug("Starting app")
+    import uvicorn
+    log.info(f"Start application (-v{APP.version}) on http://{host}:{port}")
+    uvicorn.run(APP, host=host, port=port, log_level=logging.WARNING, access_log=False)
